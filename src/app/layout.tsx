fdc40d4f90b0af58be4fddfc610ea296c9b344ec
ssik824 <64290021+victor-kiss@users.noreply.github.com>
2023-03-24 17:04:07 -0300

    update layout
    add dark mode

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({
         <meta name="p:domain_verify" content="ae78aae05c72e57e168bfb14302ac101"/>
         <title>ssik824</title>
       </head>
-      <body className={inter.className}>
+      <body className=`${inter.className} bg-dark>
         {children}
       </body>
     </html>
