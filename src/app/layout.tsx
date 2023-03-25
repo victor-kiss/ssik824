7e97e6c379ced51c9a23da04a22b1ed2f3d1d187
ssik824 <64290021+victor-kiss@users.noreply.github.com>
2023-03-25 15:39:18 -0300

    change head component
    head component changed for metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,15 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
+import './globals.css'
 import { Inter } from 'next/font/google';
-
 const inter = Inter({ subsets: ['latin'] })
 
-import './globals.css'
+export const metadata:Metadata = {
+    title:'ssik824',
+    authors:[{name:'ssik824'}],
+    description:'Venha conhecer um pouco mais sobre ssik824!',
+    generator:'victor kiss, ssik824, desenvolvedor web, front-end, web, web-design, web projects, projetos web, github projects'
+}
+
 export default function RootLayout({
   children,
 }: {
@@ -12,21 +18,8 @@ export default function RootLayout({
   return (
     <html lang="pt-br">
       <head>
-        <meta charSet="utf-8" />
-        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.svg" />
-        <meta name="keywords" content="victor kiss, ssik824, sk824,design, designer, programador,victor kiss github, vercel, ui/ux, front-end, front end, web-design, web design, github projects, victor kiss ssik824"/>
-        <meta name="author" content="ssik824"/>
-        <meta color='#242424'/>
-        <meta name="description" content="Venha explorar o mundo de ssik824!"/>
-        <meta charSet="UTF-8"/>
-        <meta httpEquiv="Content-Language" content="pt-br"/>
-        <meta name="robots" content="index, follow"/>
-        <meta name="google-site-verification" content="VhQSA-WAgNF6QY7IUMhGqwGRrRVmaGFvypFs0QZHGrM" />
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css"/>
         <meta name="p:domain_verify" content="ae78aae05c72e57e168bfb14302ac101"/>
-        <title>ssik824</title>
       </head>
       <body className={inter.className} id="bg-dark">
         {children}
