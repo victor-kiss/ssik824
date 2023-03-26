c3adc2933f9adddcadb8c7608275d6b5cd6d78f9
ssik824-smurf <128415219+ssik824-smurf@users.noreply.github.com>
2023-03-26 17:33:37 -0300

    root layout changed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,14 +2,6 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import './globals.css'
 import { Inter } from 'next/font/google';
 const inter = Inter({ subsets: ['latin'] })
-import { Metadata } from 'next'
-
-export const metadata:Metadata = {
-    title:'ssik824',
-    authors:[{name:'ssik824'}],
-    description:'Venha conhecer um pouco mais sobre ssik824!',
-    generator:'victor kiss, ssik824, desenvolvedor web, front-end, web, web-design, web projects, projetos web, github projects'
-}
 
 export default function RootLayout({
   children,
@@ -19,8 +11,13 @@ export default function RootLayout({
   return (
     <html lang="pt-br">
       <head>
+        <meta name="keywords" content="victor kiss,programador,victor kiss, ui/ux, front-end, front end, web-design, web design, github projects, victor kiss ssik824"/>  
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css"/>
         <meta name="p:domain_verify" content="ae78aae05c72e57e168bfb14302ac101"/>
+        <meta name="author" content="ssik824"/>
+        <meta name="description" content="Venha explorar o mundo de ssik824!"/>
+        <meta name="p:domain_verify" content="ae78aae05c72e57e168bfb14302ac101"/>
+
       </head>
       <body className={inter.className} id="bg-dark">
         {children}
