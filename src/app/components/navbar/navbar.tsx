00dea425f2f8e596c8d04c960ea93629342a13bb
ssik824-smurf <128415219+ssik824-smurf@users.noreply.github.com>
2023-03-29 20:05:18 -0300

    update navbar

--- src/app/components/navbar/navbar.tsx
+++ src/app/components/navbar/navbar.tsx
@@ -14,7 +14,7 @@ export default function navbar() {
                 824
             </Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" id="button">
-                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#FFFFFF" class="bi bi-list" viewBox="0 0 16 16">
+                <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="#FFFFFF" class="bi bi-list" viewBox="0 0 16 16">
                   <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"/>
                 </svg>
           </Navbar.Toggle>
