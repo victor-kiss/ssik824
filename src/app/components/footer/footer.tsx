b6d566b678f760b8cc2b45057c1e85c104dd9d2e
ssik824-smurf <128415219+ssik824-smurf@users.noreply.github.com>
2023-03-29 16:09:52 -0300

    update footer

--- src/app/components/footer/footer.tsx
+++ src/app/components/footer/footer.tsx
@@ -9,8 +9,7 @@ const FooterContainer = styled.footer`
     color:#FFF;
     padding:1rem;
 
-    i.bi-heart-fill{
-        color:#EA5455;
+    svg{
         vertical-align:middle;
     }
     span{
@@ -22,7 +21,9 @@ const FooterContainer = styled.footer`
 export default function Footer():JSX.Element{
     return (
         <FooterContainer>
-            <span>Desenvolvido com <i className="bi bi-heart-fill"></i> e <a href="https://nextjs.org/">Next.js</a></span>
+            <span>Desenvolvido com  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#EA5455" className="bi bi-heart-fill" viewBox="0 0 16 16">
+  <path fill-rule="evenodd" d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z"/>
+</svg> e <a href="https://nextjs.org/">Next.js</a></span>
         </FooterContainer>
     )
 }
