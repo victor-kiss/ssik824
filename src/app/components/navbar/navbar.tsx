65c0cadab39063954338d0ca049fdc286b51e3a0
ssik824-smurf <128415219+ssik824-smurf@users.noreply.github.com>
2023-03-29 20:03:53 -0300

    update navbar

--- src/app/components/navbar/navbar.tsx
+++ src/app/components/navbar/navbar.tsx
@@ -14,7 +14,9 @@ export default function navbar() {
                 824
             </Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" id="button">
-                <i className="bi bi-list icon text-light"></i>
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#FFFFFF" class="bi bi-list" viewBox="0 0 16 16">
+                  <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"/>
+                </svg>
           </Navbar.Toggle>
       <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
         <Nav>
