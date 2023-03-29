a201dcde8b038a5dcb83645b05d6fda587333790
ssik824-smurf <128415219+ssik824-smurf@users.noreply.github.com>
2023-03-29 14:19:17 -0300

    update main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -34,8 +34,8 @@ export default function Home() {
       <header>
         <Navbar/>
       </header>
-      <GridWrapper width="100vw" height="100vh" margin="40px 60px">
-          <FlexWrapper direction='column' align='baseline' justify='center'>
+      <GridWrapper width="100vw" height="100vh" margin="40px 60px" padding="0" mobileReverse={false}>
+          <FlexWrapper direction='column' align='baseline' justify='center' width='100%' height='auto' margin='0' padding='0' wrap='no-wrap' position='inherit'>
           <h1>Visão Geral</h1>
           <p>Meu nome é Victor Kiss, mas pode me chamar de ssik, tenho 18 anos e sou desenvolvedor front-end! Meu foco é na criação de interfaces intuitivas, com um bom design e código.Minha jornada em programação começou no ano de 2020, quando tive vontade de entender mais sobre tecnologia e me aprofundei na área de desenvolvimento web. No mesmo ano saiu meu primeiro projeto a <a href="https://victor-kiss.github.io/webdex/" rel="noreferrer" target="_blank">webdex</a>, esse projeto marca o início da minha constante evolução em front-end!</p>
           <p>Atualmente estudo sistemas para internet na  instituição Estácio de Sá e estou sempre fazendo algo relacionado a T.I, descubra mais abaixo:</p>
@@ -45,12 +45,13 @@ export default function Home() {
           </FlexWrapper>
           <CubeScene/>
       </GridWrapper>
-      <FlexWrapper width="100%" height="100vh" align='center' justify='center'>
+      <FlexWrapper width="100%" height="100vh" align='center' justify='center' direction='row'  margin='0' padding='0' wrap='no-wrap' position='inherit'>
         <Card title='Certificados' description='Descubra meus cursos realizados para ter uma base de conhecimento e aprimorar minhas habilidades.' link="/certificados"/>
-        <Card title='Conhecimentos' description='Veja todos os meus conhecimentos adquiridos e aplicados na criação dos meus projetos' link='#'/>
-        <Card title='Sites' description='Veja todos os meus projetos já realizados de forma cronológica, os projetos possuem diferentes temas e usam diferentes tecnologias.' link='#'/>
+        <Card title='Conhecimentos' description='Veja todos os meus conhecimentos adquiridos e aplicados na criação dos meus projetos' link='/conhecimentos'/>
+        <Card title='Sites' description='Veja todos os meus projetos já realizados de forma cronológica, os projetos possuem diferentes temas e usam diferentes tecnologias.' link='/sites'/>
       </FlexWrapper>
       <Footer/>
     </>
   )
 }
+
