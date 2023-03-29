211c8a65c3f84529b08bd7871dddf0cde490bc92
ssik824-smurf <128415219+ssik824-smurf@users.noreply.github.com>
2023-03-29 14:16:53 -0300

    update grid-wrapper

--- src/app/layout/grid-wrapper.tsx
+++ src/app/layout/grid-wrapper.tsx
@@ -6,7 +6,6 @@ interface StyleProps{
     margin?:string;
     padding?:string;
     color?:string;
-    column?:string;
     mobileReverse?:boolean;
 }
 
@@ -14,11 +13,10 @@ const GridWrapperContainer = styled.div<StyleProps>`
     display:grid;
     grid-template-columns:50% 50%;
     grid-template-rows:auto;
-    width:${props => props.width || "100%"};
-    height:${props => props.height || "auto"} ;
-    margin:${props => props.margin || "0px"};
-    padding:${props => props.padding || "0px"};
-    background-color:${props => props.color || "none"};
+    width:${props => props.width};
+    height:${props => props.height} ;
+    margin:${props => props.margin};
+    padding:${props => props.padding};
     backdrop-filter:blur(4px);
     -webkit-backdrop-filter:blur(4px);
     border-radius:10px;
@@ -33,29 +31,24 @@ const GridWrapperContainer = styled.div<StyleProps>`
     }
 `
 
-interface GridWrapperProps{
-    width?:string,
-    height?:string;
-    margin?:string;
-    padding?:string;
-    color?:string;
-    column?:string;
-    mobileReverse?:boolean;
-    children:React.ReactNode;
-}
 
 
-const GridWrapper:React.FC<GridWrapperProps> = ({
+const GridWrapper = ({
     width,
     height,
     margin,
     padding,
-    color,
-    column, 
     children,
     mobileReverse
+}:{
+    width:string,
+    height:string;
+    margin:string;
+    padding:string;
+    mobileReverse:boolean;
+    children:React.ReactNode;
 }) =>(
-    <GridWrapperContainer width={width} height={height} margin={margin} padding={padding} color={color} column={column} mobileReverse={mobileReverse}>
+    <GridWrapperContainer width={width} height={height} margin={margin} padding={padding}  mobileReverse={mobileReverse}>
         {children}
     </GridWrapperContainer>
 )
