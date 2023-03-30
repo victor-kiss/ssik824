6f1fd013b978f2535186482ce15e1b628786900e
ssik824-smurf <128415219+ssik824-smurf@users.noreply.github.com>
2023-03-30 17:03:30 -0300

    update /certificados

--- src/app/certificados/page.tsx
+++ src/app/certificados/page.tsx
@@ -1,19 +1,14 @@
 "use client"
 
-import Navbar from '../components/navbar/navbar'
 import GridWrapper from '../layout/grid-wrapper'
 import FlexWrapper from '../layout/flex-wrapper'
 import Card from '../components/card/card'
-import Footer from '../components/footer/footer'
 import Carousel from 'react-bootstrap/Carousel';
 import Image from 'next/image'
 
 export default function Certificados() {
   return (
     <>
-      <header>
-        <Navbar/>
-      </header>
       <GridWrapper width="100vw" height="100vh" margin="40px 60px" mobileReverse={false} padding="0">
           <FlexWrapper direction='column' align='baseline' justify='center' height='auto' margin='0' padding='0' wrap='no-wrap' position='inherit' width='auto'>
           <h1>Certificados</h1>
@@ -65,7 +60,6 @@ export default function Certificados() {
         <Card title='Conhecimentos' description='Veja todos os meus conhecimentos adquiridos e aplicados na criação dos meus projetos' link='/conhecimentos'/>
         <Card title='Sites' description='Veja todos os meus projetos já realizados de forma cronológica, os projetos possuem diferentes temas e usam diferentes tecnologias.' link='/sites'/>
       </FlexWrapper>
-      <Footer/>
     </>
   )
 }
