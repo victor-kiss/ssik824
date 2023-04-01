a5aaeb7e07f84f5d87d117c8d845a1fc0783775f
ssik824-smurf <128415219+ssik824-smurf@users.noreply.github.com>
2023-04-01 14:55:58 -0300

    Update page.tsx

--- src/app/certificados/page.tsx
+++ src/app/certificados/page.tsx
@@ -10,12 +10,12 @@ export default function Certificados() {
   return (
     <>
       <GridWrapper width="100vw" height="100vh" margin="40px 60px" mobileReverse={false} padding="0">
-          <FlexWrapper direction='column' align='baseline' justify='center' height='auto' margin='0' padding='0' wrap='no-wrap' position='inherit' width='auto'>
+          <FlexWrapper direction='column' align='baseline' justify='center' height='auto' margin='0' padding='0' wrap='no-wrap' width='auto'>
           <h1>Certificados</h1>
           <p>Aqui estão meus certificados, são frutos de todos os meus estudos, esforço e dedicação. Frequentemente uso os conhecimentos que adquiri para realizar diversas tarefas como programação, design, estudos de documentação e novas ferramentas para web dentre outras tarefas.</p>
           <p>O conteúdo dos cursos variam de acordo com meus interesses e gostos.</p>
           </FlexWrapper>
-          <FlexWrapper direction='column' align='center' justify='center' height='auto' margin='0' padding='0' wrap='no-wrap' position='inherit' width='auto'>
+          <FlexWrapper direction='column' align='center' justify='center' height='auto' margin='0' padding='0' wrap='no-wrap' width='auto'>
           <Carousel variant="dark" indicators={false}>
         <Carousel.Item>
             <Image width={650} height={450}
@@ -55,7 +55,7 @@ export default function Certificados() {
     </Carousel>
           </FlexWrapper>
       </GridWrapper>
-      <FlexWrapper width="100%" height="100vh" align='center' justify='center' margin='0' padding='0' wrap='no-wrap' position='inherit' direction='row'>
+      <FlexWrapper width="100%" height="100vh" align='center' justify='center' margin='0' padding='0' wrap='no-wrap'  direction='row'>
         <Card title='Certificados' description='Descubra meus cursos realizados para ter uma base de conhecimento e aprimorar minhas habilidades.' link="/certificados"/>
         <Card title='Conhecimentos' description='Veja todos os meus conhecimentos adquiridos e aplicados na criação dos meus projetos' link='/conhecimentos'/>
         <Card title='Sites' description='Veja todos os meus projetos já realizados de forma cronológica, os projetos possuem diferentes temas e usam diferentes tecnologias.' link='/sites'/>
