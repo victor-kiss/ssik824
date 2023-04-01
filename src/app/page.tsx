d70c9e6868f5f39ccfa719ac548652ce8150540e
ssik824-smurf <128415219+ssik824-smurf@users.noreply.github.com>
2023-04-01 12:28:35 -0300

    new purple in background button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,7 +13,7 @@ const LinkButton = styled.a`
   text-decoration:none;
   border-radius:10px;
   padding:0.8rem 1.2rem;
-  background:#696eff;
+  background:#aaadff;
   display:block;
   margin:10px 0;
   font-weight:100;
