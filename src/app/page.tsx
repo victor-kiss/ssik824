be1d29d64b6504b1302b74e86b29b1c55931b95e
ssik824-smurf <128415219+ssik824-smurf@users.noreply.github.com>
2023-04-01 19:22:32 -0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,7 +13,7 @@ const LinkButton = styled.a`
   text-decoration:none;
   border-radius:10px;
   padding:0.8rem 1.2rem;
-  background:#93aaff;
+  background:##5f81ff;
   display:block;
   margin:10px 0;
   font-weight:bold;
