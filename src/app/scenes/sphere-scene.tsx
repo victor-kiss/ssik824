603de12f32da46e7d8e564160f748db05213a294
ssik824-smurf <128415219+ssik824-smurf@users.noreply.github.com>
2023-04-05 20:37:29 -0300

    Update sphere-scene.tsx

--- src/app/scenes/sphere-scene.tsx
+++ src/app/scenes/sphere-scene.tsx
@@ -6,7 +6,7 @@ import { Preload } from '@react-three/drei'
 
 export default function SphereScene(): JSX.Element{
     return (
-        <Canvas camera={{position:[0,0,7]}} shadows={true} id="canvas-id">
+        <Canvas camera={{position:[0,0,7]}}  id="canvas-id">
             <ambientLight intensity={0.5} />
             <SphereModel />
             <Preload all={true}/>
