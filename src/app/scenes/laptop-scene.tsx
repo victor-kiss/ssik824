f15eff9fa7b72040981cf143f3b37e7ef0ae1007
ssik824-smurf <128415219+ssik824-smurf@users.noreply.github.com>
2023-04-05 20:37:05 -0300

    Update laptop-scene.tsx

--- src/app/scenes/laptop-scene.tsx
+++ src/app/scenes/laptop-scene.tsx
@@ -1,12 +1,11 @@
 import { Canvas } from '@react-three/fiber'
 import Laptop from './models-tsx/laptop'
 import { Preload } from '@react-three/drei'
-import { RandomizedLight,Center } from '@react-three/drei'
+import { Center } from '@react-three/drei'
 export default function LaptopScene(): JSX.Element{
     return (
-        <Canvas camera={{position:[0,0,20]}} shadows={true} id="canvas-scene">
+        <Canvas camera={{position:[0,0,20]}} id="canvas-scene">
             <ambientLight intensity={0.5} />
-            <RandomizedLight position={[10, 10, 10]} />
             <Center>
                 <Laptop/>
             </Center>
