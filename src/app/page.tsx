b0f2e0265cd5aa19412c261be65f847306983794
ssik824-smurf <128415219+ssik824-smurf@users.noreply.github.com>
2023-04-09 14:19:23 -0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -36,8 +36,8 @@ export default function Home() {
           <LinkButton href={process.env.GITHUB} rel="noreferrer" target="_blank">GitHub</LinkButton>
           <LinkButton href={process.env.CODEPEN} rel="noreferrer" target="_blank">Code Pen</LinkButton>
           </FlexWrapper>
-          <FlexWrapper direction='column' align='baseline' justify='center' width='100%' height='auto' margin='0' padding='0' wrap='no-wrap'>
-            <Image src="/home.gif" width={650} height={450} className="cropped" alt="home gif" />
+          <FlexWrapper direction='column' align='center' justify='center' width='100%' height='auto' margin='0' padding='0' wrap='no-wrap'>
+            <Image src="/home.gif" width={750} height={550} className="cropped" alt="home gif" />
         </FlexWrapper>
       </GridWrapper>
       <FlexWrapper width="100%" height="100vh" align='center' justify='center' direction='row'  margin='0' padding='0' wrap='no-wrap'>
