39c002c0ced51586495d3d97e539b804d2df48ac
ssik824-smurf <128415219+ssik824-smurf@users.noreply.github.com>
2023-04-16 19:30:34 -0300

    Update page.tsx

--- src/app/certificados/page.tsx
+++ src/app/certificados/page.tsx
@@ -4,13 +4,13 @@ import Carousel from 'react-bootstrap/Carousel'
 import Image from 'next/image'
 import dynamic from 'next/dynamic'
 
-export default function certificados() {
+export default function certificados(){
   const FlexWrapper = dynamic(() => import('../layout/flex-wrapper'));
   const GridWrapper = dynamic(() => import('../layout/grid-wrapper'));
   const Card = dynamic(() => import('../components/card/card'));
   return (
     <div>
-      <GridWrapper width="100vw" height="100vh" margin="40px 60px" mobileReverse={false} padding="0">
+      <GridWrapper width="100vw" height="100vh" margin="40px 60px" mobileReverse={false} padding="0" >
           <FlexWrapper direction='column' align='baseline' justify='center' height='auto' margin='0' padding='0' wrap='no-wrap' width='auto'>
           <h1>Certificados</h1>
           <p>Aqui estão meus certificados, são frutos de todos os meus estudos, esforço e dedicação. Frequentemente uso os conhecimentos que adquiri para realizar diversas tarefas como programação, design, estudos de documentação e novas ferramentas para web dentre outras tarefas.</p>
