9007f1396843be8edea6e13901082fa98aec8e16
ssik824-smurf <128415219+ssik824-smurf@users.noreply.github.com>
2023-04-16 22:21:44 -0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,8 +15,8 @@ export default function Home() {
           <p>Meu nome é Victor Kiss, mas pode me chamar de ssik, tenho 18 anos e sou desenvolvedor front-end! Meu foco é na criação de interfaces intuitivas, com um bom design e código.Minha jornada em programação começou no ano de 2020, quando tive vontade de entender mais sobre tecnologia e me aprofundei na área de desenvolvimento web. No mesmo ano saiu meu primeiro projeto a <a href="https://victor-kiss.github.io/webdex/" rel="noreferrer" target="_blank">webdex</a>, esse projeto marca o início da minha constante evolução em front-end!</p>
           <p>Atualmente estudo sistemas para internet na  instituição Estácio de Sá e estou sempre fazendo algo relacionado a T.I, descubra mais abaixo:</p>
 
-          <a href={process.env.GITHUB} rel="noreferrer" target="_blank" rel="noreferrer" className="link-button">GitHub</a>
-          <a href={process.env.CODEPEN} rel="noreferrer" target="_blank" rel="noreferrer" className="link-button">Code Pen</a>
+          <a href={process.env.GITHUB} rel="noreferrer" target="_blank" className="link-button">GitHub</a>
+          <a href={process.env.CODEPEN} rel="noreferrer" target="_blank" className="link-button">Code Pen</a>
           </FlexWrapper>
           <FlexWrapper direction='column' align='center' justify='center' width='100%' height='auto' margin='0' padding='0' wrap='no-wrap'>
             <Image src="/ssik1.png" width={800} height={600} className="cropped" alt="home image" loading={'eager'}/>
