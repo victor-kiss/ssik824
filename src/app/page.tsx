0a80c832e5ae21b837d97c19a40ccfc3113b2870
ssik824-smurf <128415219+ssik824-smurf@users.noreply.github.com>
2023-04-16 22:14:12 -0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,23 +3,7 @@ import dynamic from 'next/dynamic';
 import styled from 'styled-components';
 import Image from 'next/image';
 
-const LinkButton = styled.a`
-  color:#FFF;
-  text-decoration:none;
-  border-radius:10px;
-  padding:0.8rem 1.2rem;
-  background:#712CF9;
-  display:block;
-  margin:10px 0;
-  font-weight:100;
 
-  &:hover{
-   font-weight:bold;
-   transition:.1s ease-in-out;
-   color:#FFF;
-   letter-spacing:1px;
-  }
-`
 
 export default function Home() {
   const FlexWrapper = dynamic(() => import('./layout/flex-wrapper'));
@@ -33,8 +17,8 @@ export default function Home() {
           <p>Meu nome é Victor Kiss, mas pode me chamar de ssik, tenho 18 anos e sou desenvolvedor front-end! Meu foco é na criação de interfaces intuitivas, com um bom design e código.Minha jornada em programação começou no ano de 2020, quando tive vontade de entender mais sobre tecnologia e me aprofundei na área de desenvolvimento web. No mesmo ano saiu meu primeiro projeto a <a href="https://victor-kiss.github.io/webdex/" rel="noreferrer" target="_blank">webdex</a>, esse projeto marca o início da minha constante evolução em front-end!</p>
           <p>Atualmente estudo sistemas para internet na  instituição Estácio de Sá e estou sempre fazendo algo relacionado a T.I, descubra mais abaixo:</p>
 
-          <LinkButton href={process.env.GITHUB} rel="noreferrer" target="_blank">GitHub</LinkButton>
-          <LinkButton href={process.env.CODEPEN} rel="noreferrer" target="_blank">Code Pen</LinkButton>
+          <a href={process.env.GITHUB} rel="noreferrer" target="_blank" rel="noreferrer" className="link-button">GitHub</an>
+          <a href={process.env.CODEPEN} rel="noreferrer" target="_blank" rel="noreferrer className="link-button">Code Pen</a>
           </FlexWrapper>
           <FlexWrapper direction='column' align='center' justify='center' width='100%' height='auto' margin='0' padding='0' wrap='no-wrap'>
             <Image src="/ssik1.png" width={800} height={600} className="cropped" alt="home image" loading={'eager'}/>
