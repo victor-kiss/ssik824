adfe1b7b36651a39a09a75b2fe16c769b06cbe14
ssik824-smurf <128415219+ssik824-smurf@users.noreply.github.com>
2023-04-23 11:53:03 -0300

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,3 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './globals.min.css';
 import Navbar from './components/navbar/navbar';
 import { Inter } from 'next/font/google';
 import { Metadata } from 'next';
@@ -22,6 +20,9 @@ export default function RootLayout({
     <html lang="pt-br">
       <head>
         <meta name="p:domain_verify" content="ae78aae05c72e57e168bfb14302ac101"/>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossOrigin="anonymous"/> 
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossOrigin="anonymous"></script>
+        <link rel='stylesheet' href='/globals.css'/>
       </head>
       <body className={inter.className} id="bg-dark">
       <header>
