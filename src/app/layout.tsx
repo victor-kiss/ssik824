cd43c0eb279e6d344935d2002b0c127e0917fcea
ssik824-smurf <128415219+ssik824-smurf@users.noreply.github.com>
2023-04-26 19:26:57 -0300

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,9 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './globals.css';
 import Navbar from './components/navbar/navbar';
 import { Inter } from 'next/font/google';
+import Link from 'next/link';
 import { Metadata } from 'next';
+
 
 const inter = Inter({ subsets: ['latin'] });
 export const metadata:Metadata = {
@@ -28,6 +30,23 @@ export default function RootLayout({
         <Navbar/>
       </header>
         {children}
+        <div className='flex-wrapper'>
+          <div className='card'>
+              <span>Certificados</span>
+              <p>Descubra meus cursos realizados para ter uma base de conhecimento e aprimorar minhas habilidades.</p>
+              <Link href="/certificados">Ver</Link>
+          </div>
+          <div className='card'>
+              <span>Conhecimentos</span>
+              <p>Veja todos os meus conhecimentos adquiridos e aplicados na criação dos meus projetos</p>
+              <Link href="/conhecimentos">Ver</Link>
+          </div>
+          <div className='card'>
+              <span>Sites</span>
+              <p>Veja todos os meus projetos já realizados de forma cronológica, os projetos possuem diferentes temas e usam diferentes tecnologias</p>
+              <Link href="/sites">Ver</Link>
+          </div>
+        </div>
         <footer className='footer'>
         <span>Desenvolvido com  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#EA5455" className="bi bi-heart-fill" viewBox="0 0 16 16">
         <path fillRule="evenodd" d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z"/>
