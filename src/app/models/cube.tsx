ad86e7ab0eb5b338e92187eaac1b1fc30eff0c34
ssik824-smurf <128415219+ssik824-smurf@users.noreply.github.com>
2023-05-31 13:06:45 +0000

    src files modified

---
+++ src/app/models/cube.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { useFrame} from '@react-three/fiber';
+import { Box } from '@react-three/drei';
+
+
+export default function Cube(){
+    const ref = React.useRef<THREE.Mesh>(null!)
+    useFrame(() =>{
+        ref.current.rotation.y += 0.002;
+    })
+    return(
+    <mesh scale={1.5} ref={ref}>
+        <Box args={[2,2,2]} >
+        <meshBasicMaterial color="#712CF9" wireframe />
+      </Box>
+    </mesh>
+    )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,14 @@
 "use client"
 import dynamic from 'next/dynamic';
 import { Canvas } from '@react-three/fiber';
-import { CameraControls, OrbitControls, OrthographicCamera, Preload} from '@react-three/drei';
+import {Preload} from '@react-three/drei';
+
 
 
 export default function Home() {
   const FlexWrapper = dynamic(() => import('./layout/flex-wrapper'));
   const GridWrapper = dynamic(() => import('./layout/grid-wrapper'));
-  const Molang = dynamic(() => import('./models/molang'));
+  const Cube = dynamic(() => import('./models/cube'));
   return (
     <>
       <GridWrapper width="100vw" height="100vh" margin="40px 60px" padding="0" mobileReverse={true}>
@@ -20,8 +21,10 @@ export default function Home() {
           <a href={process.env.CODEPEN} rel="noreferrer" target="_blank" className="link-button">Code Pen</a>
           </FlexWrapper>
           <FlexWrapper direction='column' align='center' justify='center' width='100%' height='100%' margin='0' padding='0' wrap='no-wrap'>
-          <Canvas camera={{position:[5,0,20]}} frameloop="demand" >
-          <Molang />
+          <Canvas camera={{position:[5,0,5]}}>
+          <ambientLight intensity={0.10} color={'#f3f4f5'}/>
+         
+          <Cube />
           <Preload all={true}/>
           </Canvas>
           </FlexWrapper>
@@ -29,4 +32,3 @@ export default function Home() {
     </>
   )
 }
-
