10b59bf7e5e75fe7d56b0e925ea5868b4e0e0a83
ssik824-smurf <128415219+ssik824-smurf@users.noreply.github.com>
2023-06-06 22:28:20 +0000

    removed ancora tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -48,7 +48,7 @@ export default function RootLayout({
         <footer className='footer'>
         <span>Desenvolvido com  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#EA5455" className="bi bi-heart-fill" viewBox="0 0 16 16">
         <path fillRule="evenodd" d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z"/>
-        </svg> e <a href="https://nextjs.org/" rel="noreferrer" target="_blank" >Next.js. </a></span>
+        </svg> e <Link href="https://nextjs.org/" rel="noreferrer" target="_blank" >Next.js. </Link></span>
         </footer>
       </body>
     </html>
